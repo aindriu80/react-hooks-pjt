3aab92458ee8eefc54f1ef466bbd54c9ad0c7540
aindriu <aindriu80@gmail.com>
2019-11-09 21:02:41 +0000

    Using the useState() Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div className="App">
-
+      <Todo />
     </div>
   );
 }
 
 export default App;
+
